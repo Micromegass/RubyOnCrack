470421b5f519b735877e56c9bfefdbb23e1ce1f8
axelbraunschweiger <axelbraunschweiger@gmail.com>
2018-06-24 11:59:14 +0200

    added delete, update methods

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -21,16 +21,34 @@ class QuestionsController < ApplicationController
 
 
     def edit
-      
+      @question = Question.find(params[:id])
     end
 
     def update
-    
+      @question = Question.find(params[:id])
+      if @question.update(questions_params)
+      flash[:success] = "Bravo, you updated your question!"
+      redirect_to root_path
+      else
+      flash[:alert] = "Ups, something went wrong. Please try again..."
+      render :new
+      end
     end
 
-private
-    def questions_params
-        params.require(:question).permit(:title, :description)
-    end 
+
+    def destroy
+     @question = Question.find(params[:id])
+      if @question.destroy
+      redirect_to root_path, notice: "Ok, question deleted"
+      else 
+      flash[:alert] = "Ups, something went wrong. Please try again..."
+      redirect_to root_path
+      end 
+    end
+
+    private
+        def questions_params
+            params.require(:question).permit(:title, :description)
+        end 
 
 end

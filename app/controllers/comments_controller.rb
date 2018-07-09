a4abbb785f953c029c830486ab754e7404dd7ba0
axelbraunschweiger <axelbraunschweiger@gmail.com>
2018-07-09 16:34:44 +0200

    did some styling

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,16 +4,21 @@ class CommentsController < ApplicationController
       @comment = Comment.new(comment_params)
       if @comment.save
         flash[:success] = "Thanks for commenting"
-        redirect_to root_path
       else 
       flash[:danger] = @comment.errors.full_messages.to_sentence
-      end
-
     end
 
+        if comment_params[:commentable_type] == "Question"
+          redirect_to question_path(comment_params[:commentable_id])
+        else 
+          @answer = Answer.find(comment_params[:commentable_id])
+  			  redirect_to question_path(@answer.question_id)
+        end 
 
+    end
+  	
     private
-    
+  
     def comment_params
         params.require(:comment).permit(:body, :commentable_id, :commentable_type, :user_id)
     end 
--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ class User < ApplicationRecord
 
     has_many :answers, dependent: :destroy
     has_many :questions, dependent: :destroy
-    has_many :comments
+    has_many :comments, dependent: :destroy
 
 
 
